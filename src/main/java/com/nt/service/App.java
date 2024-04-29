adbc05bd6fb6a04b8fa54cc1555a4269eaa86dc8
shailaja <donthulashailaja@gmail.com>
2024-04-29 14:00:47 +0530

    intial commit

---
+++ src/main/java/com/nt/service/App.java
@@ -0,0 +1,19 @@
+package com.nt.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public int sum(int x, int y) {
+	System.out.println("X= "+x+" Y= "+y);
+		return x+y;
+	}
+    public static void main( String[] args )
+    {
+    	App app = new App();
+    	System.out.println("sum is "+app.sum(10,20));
+    }
+    
+}
